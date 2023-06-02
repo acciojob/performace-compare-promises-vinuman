0c3ce058894f2afe7233e8fef1f662f1e9467ea2
vinu cyril <38722628+vinuman@users.noreply.github.com>
2023-06-02 15:52:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,4 +12,25 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
 
+function apiCall(apiUrls){
+	const startTime = performance.now();
+	Promise.all(apiUrls.map((url) =>{
+			fetch(url).then(response => response.json())
+		.then((data) =>{
+			const endTime = performance.now();
+            const totalTime = endTime - startTime;
+			const outputAll = document.getElementById('output-all');
+			const td = document.createElement("td");
+			td.textContent = totalTime;
+			outputAll.appendChild(td);
+		})
+		}));
+
+	Promise.any(apiUrls.map((url) =>{
+			fetch(url).then(response => response.json());
+		}))
+}
+
+apiCall(apiUrls);
+
 // You can write your code here
